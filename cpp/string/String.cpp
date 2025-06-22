9d9f0b49ba14fca56449f9c420b729ea4f125da8
taoyanqi <yqtao@whu.edu.cn>
2025-06-22 18:08:55 +0800

    others/

---
+++ cpp/string/String.cpp
@@ -0,0 +1,5 @@
+//
+// Created by taoyanqi on 2020/10/24.
+//
+
+#include "String.h"
---
+++ cpp/string/String.h
@@ -0,0 +1,16 @@
+//
+// Created by taoyanqi on 2020/10/24.
+//
+
+#ifndef CPP_STRING_H
+#define CPP_STRING_H
+
+
+
+class String {
+
+};
+
+
+
+#endif //CPP_STRING_H
